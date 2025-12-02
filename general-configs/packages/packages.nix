6c5b717071affba797a6ca1bfad593103520d983
vulkce <vulkce@proton.me>
2025-12-02 00:37:28 -0300

    unified flatpak.nix in packages.nix

--- general-configs/packages/packages.nix
+++ general-configs/packages/packages.nix
@@ -1,6 +1,8 @@
 { config, lib, pkgs, inputs, ... }: {
 
-    environment.systemPackages = with pkgs; [
+# -------- SYSTEM --------
+
+  environment.systemPackages = with pkgs; [
 
     # coisas úteis
     usbutils
@@ -31,9 +33,33 @@
     hyprpaper
     waybar
     labwc
+];
 
-#    inputs.zen-browser.packages.${system}.default # zen-browser
+# -------- FLATPAK --------
 
-   ];
+  services.flatpak.enable = true; # habilita a flatpak
+
+  # repositórios usados para instalar aplicativos
+  services.flatpak.remotes = [
+    
+    { name = "flathub"; location = "https://flathub.org/repo/flathub.flatpakrepo"; }
+    { name = "gnome-nightly"; location = "https://nightly.gnome.org/gnome-nightly.flatpakrepo"; } 
+    { name = "elementaryos"; location = "https://flatpak.elementary.io/repo.flatpakrepo"; }  
+    { name = "PureOS"; location = "https://store.puri.sm/repo/stable/pureos.flatpakrepo"; }
+  
+  ];
+  
+  # aplicativos declarados
+  services.flatpak.packages = [
+    { appId = "app.zen_browser.zen"; origin = "flathub"; }
+              "org.vinegarhq.Sober"
+  ];
+
+  # atualização automática:
+  services.flatpak.update.onActivation = true;
+  services.flatpak.update.auto = {
+    enable = true;
+    onCalendar = "weekly";
+  };
 
 }
