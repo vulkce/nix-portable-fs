2e6cf4c8c0304e1b230850f5b68dd9a065d6d9e7
vulkce <vulkce@proton.me>
2025-12-14 20:46:19 -0300

    refactor: system.nix

--- general-configs/system/system.nix
+++ general-configs/system/system.nix
@@ -1,21 +1,51 @@
-{ config, lib, pkgs, modulesPath, ... }: {
+{ config, lib, pkgs, modulesPath, ... }: 
+{
 
 # -------- NIXOS --------
 
   # define e configura options do boot
   boot = {
+    # kernel e boot
     kernelPackages = pkgs.linuxPackages_lqx; # kernel LQX, kernel com foco em baixa latencia e gaming
-    kernelModules = [ "kvm-amd" "hid_playstation" "hid_sony" "uinput" ]; # modulos do kernel
-    supportedFilesystems = [ "zfs" ]; # filesystems extras
+    kernelModules = [ # modulos do kernel
+      "kvm-amd" 
+      "hid_playstation" 
+      "hid_sony" 
+      "uinput" 
+    ];
+    kernelParams = [
+      "idle=poll" # pode reduzir latencia
+      "amd_pstate=active" # o hardware controla
+      # "isolcpus=<cpus>" # isola cores.
+    ];
+    initrd.availableKernelModules = [ 
+      "xhci_pci" 
+      "ahci" 
+      "usbhid" 
+      "usb_storage" 
+      "sd_mod" 
+    ];
+    initrd.kernelModules = [ 
+      "dm-snapshot" 
+    ];
+    extraModulePackages = [ ];
     loader.systemd-boot.enable = true; # usa systemd-boot
+    # zfs
+    supportedFilesystems = [ "zfs" ]; # filesystems extras
     zfs.removeLinuxDRM = true; # protege o zfs caso futuras atts de kernels quebrem o zfs por conta da GPL
   };
 
-  # nome do sistema
+  # options do networking
   networking = {
     hostName = "flake"; # configura o hostname 
     hostId = "8bec9fba"; # configura o hostId para o zfs
     networkmanager.enable = true; # usa o networkmanager
+    useDHCP = lib.mkDefault true; # usa o DHCP
+    firewall = {
+      enable = true;
+      allowedTCPPorts = [ 80 4580 9090 ];
+      allowedUDPPorts = [ ];
+    };
   };
 
   # pipewire
@@ -23,23 +53,6 @@
     enable = true;
     pulse.enable = true; # ativa compatibilidade com pulseaudio
     jack.enable = true; # ativa compatibilidade com o jack
-  };
-
-  # habilita o bluetooth
-  hardware.bluetooth = {
-    enable = true;
-    powerOnBoot = true; # inicia o bluetooth no boot
-    settings.General = {
-      ControllerMode = "bredr";
-      Experimental = true; # mostra mais informações sobre o dispositivo
-    };
-  };
-
-  # firewall
-  networking.firewall = {
-    enable = true;
-    allowedTCPPorts = [ 80 4580 9090 ];
-    allowedUDPPorts = [ ];
   };
 
   nix = {
@@ -54,17 +67,18 @@
     settings = { # configura o uso de várias threads
       max-jobs = "auto"; # usa todos os cores
       cores = 0; # distribui a carga
+      experimental-features = [ "nix-command" "flakes" ]; # experimental
     };
+    
+  };
+
+  nixpkgs = {
+    hostPlatform = lib.mkDefault "x86_64-linux"; # faz o nixpkgs seguir a arch da CPU
+    config.allowUnfree = true; # permite pacotes unfree
   };
 
   # timezone
   time.timeZone = "America/Sao_Paulo";
-
-  # experimental
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-  # unfree
-  nixpkgs.config.allowUnfree = true;
 
   # versão no qual a primeira build foi feita!
   system.stateVersion = "25.11";
@@ -76,12 +90,6 @@
     cpuFreqGovernor = "performance";
   };
 
-  boot.kernelParams = [
-    "idle=poll" # pode reduzir latencia
-    "amd_pstate=active" # o hardware controla
-    # "isolcpus=<cpus>" # isola cores.
-  ];
-
   # habilita zram
   zramSwap = {
     enable = true;
@@ -90,34 +98,25 @@
     priority = 5; # preferencia pela zram
   };
 
-  # configura a GPU corretamente
   hardware = {
+    # cpu microcode
+    cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+    # configura GPU corretamente
     amdgpu.opencl.enable = true;
     graphics = {
       enable = true;
       enable32Bit = true;
     };
+    # bluetooth
+    bluetooth = {
+      enable = true;
+      powerOnBoot = true; # inicia o bluetooth no boot
+      settings.General = {
+        ControllerMode = "bredr";
+        Experimental = true; # mostra mais informações sobre o dispositivo
+      };
+    };
   };
 
-  # swap normal
-#  swapDevices = [
-#    {
-#      device = "/dev/disk/by-uuid/d2b636cc-bf0e-4fb8-8448-ee032ebfdc8d"; # sempre usem UUIDs!!!
-#      priority = 0; # usa o swap quando a zram encher
-#    }
-#  ];
-
-  imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
-    ];
-
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
-  boot.initrd.kernelModules = [ "dm-snapshot" ];
-  boot.extraModulePackages = [ ];
-
-  networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp3s0.useDHCP = lib.mkDefault true;
-
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 }
